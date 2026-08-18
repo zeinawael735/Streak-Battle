ea86e51f354c4b599f2b7de1d1a303b6fbbf78ae
Omar Badwey <om44716030@gmail.com>
2026-08-19 01:51:43 +0300

    add button icon for back

--- lib/features/battle_detailes/view/screens/battle_details_screen.dart
+++ lib/features/battle_detailes/view/screens/battle_details_screen.dart
@@ -7,7 +7,7 @@ class BattleDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: Icon(Icons.arrow_back_ios_new),),
+      appBar: AppBar(leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_ios_new)),),
       backgroundColor: AppColorStyle.scaffoldBackgroundColor,
       body: Center(child: Text("Battle details screen"),),
     );
