32c13a4449890cb957ed2c3ab53e1f5370bb0192
Omar Badwey <om44716030@gmail.com>
2026-08-20 22:29:25 +0300

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:streak_battle/features/battle/view/screens/battle_details_screen.dart';
-
+import 'core/theme/theme.dart';
 import 'core/helper/auth_helper.dart';
 import 'core/routes/app_routes.dart';
 import 'features/app_section/view/screens/app_section.dart';
 import 'features/auth/login/view/screens/login_screen.dart';
 import 'features/auth/signup/view/screens/signup_screen.dart';
 import 'features/battle/view/screens/create_battle_screen.dart';
+import 'features/battle/view/screens/join_battle_screen.dart';
 import 'firebase_options.dart';
 import 'features/notification/view_model/notification_services.dart';
 
@@ -36,9 +37,9 @@ class BattleStreakApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Streak Battle',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF131313),
-      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
       initialRoute: initialRoute,
       routes: {
         AppRoutes.appSection: (context) => const AppSection(),
@@ -46,6 +47,7 @@ class BattleStreakApp extends StatelessWidget {
         AppRoutes.signUp: (context) => const SignupScreen(),
         AppRoutes.login: (context) => const LoginScreen(),
         AppRoutes.battleDetails: (context) => const BattleDetailsScreen(),
+        AppRoutes.joinBattle: (context) => const JoinBattleScreen(),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
