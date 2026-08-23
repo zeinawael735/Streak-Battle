e4a63eb7a3393d183f6dcd16dc99505f8d3f9426
Zeina <zeina.wael735@gmail.com>
2026-08-24 00:42:09 +0300

    fcm

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,22 @@ import 'features/auth/login/view/screens/login_screen.dart';
 import 'features/auth/signup/view/screens/signup_screen.dart';
 import 'features/battle/view/screens/create_battle_screen.dart';
 import 'features/battle/view/screens/join_battle_screen.dart';
+import 'features/notification/view_model/push_notification_service.dart';
 import 'firebase_options.dart';
 import 'features/notification/view_model/notification_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.init();
+  //await NotificationService.init();
 
   if (Firebase.apps.isEmpty) {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
   }
+
+  PushNotificationService notificationService = PushNotificationService();
+  await notificationService.initialize();
 
   final String initialRoute = await AuthHelper.getInitialRoute();
 
@@ -40,6 +44,7 @@ class BattleStreakApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: PushNotificationService.navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Streak Battle',
       theme: AppTheme.lightTheme,
